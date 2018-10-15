2e02b63f0d20c0de3eaaff5674a91fb2de714cad
Brian Camacho <hello@briancamacho.me>
2018-10-15 19:56:38 +0100

    -

--- q2.py
+++ q2.py
@@ -2,9 +2,32 @@
 
 import numpy as np
 
-#compute all combinations for two portfolios
-def question02(cashFlowIn, cashFlowOut):
-  # modify and then return the variable below
-  answer = -1
-  return answer
 
+# compute all combinations for two portfolios
+
+def question02(cashFlowIn, cashFlowOut):
+    # modify and then return the variable below
+
+    larger, smaller = [], []
+    if len(cashFlowIn) >= len(cashFlowOut):
+        larger, smaller = sorted(cashFlowIn, reverse=True), sorted(cashFlowOut, reverse=True)
+    else:
+        smaller, larger = sorted(cashFlowIn, reverse=True), sorted(cashFlowOut, reverse=True)
+
+    # consider just choosing smallest number
+    if larger[len(larger) - 1] >= smaller[len(smaller) - 1]:
+        larger.append(0)
+    else:
+        smaller.append(0)
+
+    # prealloc for less resizing
+    ds = [-1] * len(smaller)
+
+    i, j, cur = 0, 0, 0
+    while i < len(smaller):
+        j = i
+        while j < len(larger):
+
+
+    answer = -1
+    return answer
