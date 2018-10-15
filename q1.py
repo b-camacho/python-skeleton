1e929bbc86eff3daed8e65517a5cf42814ef8c19
Brian Camacho <hello@briancamacho.me>
2018-10-15 15:13:45 +0100

    test brute

--- q1.py
+++ q1.py
@@ -2,8 +2,20 @@
 
 import numpy as np
 
+
+def brute(portfolios):
+    cur_max = 0
+    for i in range(0, len(portfolios)):
+        for j in range(i, len(portfolios)):
+            cur_max = max(cur_max, portfolios[i] ^ portfolios[j])
+
+    return cur_max
+
+
 # modify this function, and create other functions below as you wish
+
 def question01(portfolios):
-  # modify and then return the variable below
-  answer = 0
-  return answer
+    # modify and then return the variable below
+    answer = brute(portfolios)
+    return answer
+
